f8330e70a1721e65a7fe4dd9a9b69eed17edfe3c
JeongSikKim <JeongSikKim@bitbucket.org>
2020-06-07 22:40:01 +0900

    array and slice

--- main.go
+++ main.go
@@ -4,14 +4,8 @@ import (
 	"fmt"
 )
 
-func canIDrink (age int) bool {
-	if koreanAge := age +2; koreanAge <= 18{
-		return false
-	}
-	return true
-
-}
-
 func main() {
-	fmt.Println(canIDrink(16))
+	names := []string{"jack", "gonyon", "surl"}
+	names = append(names, "leo")
+	fmt.Println(names)
 }

e1621f5405e8685576d16e556546fe11782ad1b1
JeongSikKim <JeongSikKim@bitbucket.org>
2020-06-20 20:21:40 +0900

    Add Method

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/JeongSikKim88/go-jobscrapper/mydict"
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(definition)
+
+	hello, _ := dictionary.Search(word)
+	fmt.Println(hello)
+
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,14 +5,28 @@ import (
 )
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That is already exists ")
 
 // Dictionary type
 type Dictionary map[string]string
 
+// Search for a word
 func (d Dictionary) Search(word string) (string, error) {
 	value, exists := d[word]
 	if exists {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }

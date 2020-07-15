0d596ca0642b4a8dbb19cec552815a8f67edd952
JeongSikKim <JeongSikKim@bitbucket.org>
2020-07-16 00:50:22 +0900

    channel

--- main.go
+++ main.go
@@ -6,13 +6,25 @@ import (
 )
 
 func main() {
-	go sexyCount("nico")
-	sexyCount("jack")
+	c := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	
+	// you have two way for receving two chan msg
+	
+	// #1
+	// result := <-c
+	// fmt.Println(result)
+
+	// #2
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
 }

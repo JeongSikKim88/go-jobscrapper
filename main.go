d3a23c0cd2983081cfa422df97a1ceb8f293e9b0
JeongSikKim88 <jackslash88@gmail.com>
2020-09-15 03:50:01 +0900

    go channel

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	c := make(chan bool)
+	c := make(chan string)
 	people := [2]string{"nico", "flynn"}
 	for _, person := range people {
 		go isSexy(person, c)
@@ -19,12 +19,14 @@ func main() {
 	// fmt.Println(result)
 
 	// #2
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	fmt.Println("Waiting for a message")
+	resultOne := <-c
+	resultTwo := <-c
+	fmt.Println("Received this message : ", resultOne)
+	fmt.Println("Received second message : ", resultTwo)
 }
 
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	c <- true
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	c <- person + " is Sexy"
 }
